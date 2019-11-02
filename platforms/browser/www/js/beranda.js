c31dfa608d8e521b148d8e296404ff02c14f0718
root <root@srv.haxors.or.id>
2019-11-02 19:24:20 +0000

    sudah

---
+++ platforms/browser/www/js/beranda.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  
+          $('.materialboxed').materialbox();
+          $('.button-collapse').sideNav();
+          $('select').material_select();
+          var loading = "<div class='progress'><div class='indeterminate'></div></div>";
+          console.log("Halaman berhasil di load");
+          $('#divUtama').html(loading);
+          $('#divUtama').load('berandaUtama.php');
+          $('#judulApps').html("Track Rute Apps");
+      //tombol beranda
+      $('#homeSideNav').click(function(){
+      location.reload();
+      });
+  
+  $('#pesanDriver').click(function(){
+    
+            $('#divUtama').html(loading);
+          $('#divUtama').load('pesanDriver.php');
+          $('#judulApps').html("Track Rute Apps");
+    $('.button-collapse').sideNav('hide');
+  });
+  
+  
+  $('#pesananSaya').click(function(){    
+            $('#divUtama').html(loading);
+          $('#divUtama').load('pesananSaya.php');
+          $('#judulApps').html("Track Rute Apps");
+    $('.button-collapse').sideNav('hide');
+  });
+  
+  
+      
+    
+  
+      function updateSistem(){
+        Materialize.toast('Server realtime sedang berjalan', 1100);
+      }
+  
+     
+      
+    });
--- platforms/browser/www/js/login.js
+++ platforms/browser/www/js/login.js
@@ -2,20 +2,39 @@ $(document).ready(function(){
  
   let username = $('#txtUsername');
   let password = $('#txtPassword');
- 
+  username.focus();
   let statusKoneksi = navigator.onLine;
-      
-  if(statusKoneksi === true){
+  let statusServer = '';
+ 
+    if(statusKoneksi === true){
      $('#capStatus').html('Status koneksi ke server : terhubung');
-  }else{
-    $('#capStatus').html('Status koneksi ke server : tidak terhubung');
-  }
-     
+      }else{
+        $('#capStatus').html('Status koneksi ke server : tidak terhubung. Melakukan koneksi ulang dalam 5 detik.');
+      }
   
   $('#btnLogin').click(function(){
-    $.get('http://157.245.154.248:3563/anggota/all',function(data){
-      console.log(data);
-    });
+    let cekLagi = navigator.onLine;
+    if(cekLagi === true){
+      let userId = username.val();
+      let passId = password.val();
+      if(userId ==='' || passId === ''){
+        window.alert("Isi username & password ..");
+        username.focus();
+      }else{
+        $('#capStatus').html("Login ...");
+        $.post('http://157.245.154.248:3565/login-user',{'userId':userId,'passId':passId},function(data){
+          let status = data.status;
+          if(status == 'sukses'){
+            window.location.assign('mainApp/beranda.html');
+          }else{
+            window.alert("Username / password yang kamu masukkan salah..");
+          }
+        });
+      }
+    }else{
+      window.alert("Tidak ada koneksi, harap periksa koneksi");
+    }
+   
   });
   
 });
