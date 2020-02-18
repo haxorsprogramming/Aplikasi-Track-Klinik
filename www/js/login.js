6ee3d3d86c1c45216066e9a53a3c02ee1f8f29a3
haxorsprogramming <haxorsuinsu@gmail.com>
2020-02-18 18:32:52 +0700

    aaa

--- www/js/login.js
+++ www/js/login.js
@@ -15,24 +15,7 @@ $(document).ready(function(){
   $('#btnLogin').click(function(){
     let cekLagi = navigator.onLine;
     if(cekLagi === true){
-      let userId = username.val();
-      let passId = password.val();
-      if(userId ==='' || passId === ''){
-        window.alert("Isi username & password ..");
-        username.focus();
-      }else{
-        $('#capStatus').html("Login ...");
-        $.post('http://api.haxors.or.id/taing/login_cek.php',{'userId':userId,'passId':passId},function(data){
-          let obj = JSON.parse(data);
-          let status = obj.status;
-          console.log(data);
-          if(status == 'sukses'){
-            window.location.assign('mainApp/beranda.html');
-          }else{
-            window.alert("Username / password yang kamu masukkan salah..");
-          }
-        });
-      }
+      window.location.assign('mainApp/beranda.html');
     }else{
       window.alert("Tidak ada koneksi, harap periksa koneksi");
     }
